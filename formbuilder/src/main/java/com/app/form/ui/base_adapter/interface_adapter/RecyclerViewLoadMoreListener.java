6dcdea3c36af179ba40adddbb53ac2943d9d5878
Viet Anh <dinhanh138@gmail.com>
2022-08-11 10:39:08 +0700

    init app

--- formbuilder/src/main/java/com/app/form/ui/base_adapter/interface_adapter/RecyclerViewLoadMoreListener.java
+++
@@ -1,5 +0,0 @@
-package com.app.form.ui.base_adapter.interface_adapter;
-
-public interface RecyclerViewLoadMoreListener {
-    void onLoad(int index);
-}
--- formbuilder/src/main/java/com/app/form/ui/valiadation/EdittextImeOptionsListener.java
+++
@@ -1,5 +0,0 @@
-package com.app.form.ui.valiadation;
-
-public interface EdittextImeOptionsListener {
-     void onClick(int imeOptions);
-}
